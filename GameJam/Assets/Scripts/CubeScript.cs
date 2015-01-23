a1a1a6e999e02a15aa6b288fb6d17086f6078096
TammiLion <trp300@gmail.com>
2015-01-23 23:24:23 +0100

    Cube animation and type init
    Started working on types and animations of cubes.

--- GameJam/Assets/Scripts/CubeScript.cs
+++ GameJam/Assets/Scripts/CubeScript.cs
@@ -2,8 +2,23 @@
 using System.Collections;
 
 public class CubeScript : MonoBehaviour {
-	
 
+	enum Type {
+		melee,
+		ranged,
+		tank
+	}
+
+	enum Element {
+		fire,
+		water,
+		earth,
+		electricity
+	}
+
+	int health;
+	Type type;
+	Element element;
 
 	// Use this for initialization
 	void Start() {
