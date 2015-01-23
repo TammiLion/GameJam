9b045c9afd72e50a82d83cc7600e4dadde9fdddb
TammiLion <trp300@gmail.com>
2015-01-23 22:51:55 +0100

    TeamScript edit
    Blocks are now set.

--- GameJam/Assets/Scripts/CubeScript.cs
+++ GameJam/Assets/Scripts/CubeScript.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class CubeScript : MonoBehaviour {
+	
+
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
