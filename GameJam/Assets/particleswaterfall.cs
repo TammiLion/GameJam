3aa5ee952a1904e6b4d6df447cfafc8db4eb3743
dimitris dimitriadis <melkorinos@gmail.com>
2015-01-25 00:43:11 +0100

    Everything of art and explosion prefab
    what i said in title

---
+++ GameJam/Assets/particleswaterfall.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particleswaterfall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.sortingLayerName = "Rest"; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
