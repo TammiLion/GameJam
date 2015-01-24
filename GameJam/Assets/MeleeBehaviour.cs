865399966911593fb1ed3eede38e066b06e94b57
RobertBroersma <Robert_broersma@hotmail.com>
2015-01-24 15:56:43 +0100

    Playerbehaviours + Cubebehaviour + Childclasses

--- GameJam/Assets/MeleeBehaviour.cs
+++ GameJam/Assets/MeleeBehaviour.cs
@@ -2,15 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class RangedBehaviour : CubeBehaviour {
-	
+public class MeleeBehaviour : CubeBehaviour {
+	private float attackSpeed = 2;
+
 	// Update is called once per frame
 	void Update () {
 		bulletTimer += Time.deltaTime;
 	}
 
 	public override void Attack() {
-		if (HP > 0 && bulletTimer >= 3){
+		if (HP > 0 && bulletTimer >= 2){
 			Transform bullet = Instantiate(bulletPrefab, new Vector3(transform.position.x + 1f, transform.position.y, 0f), Quaternion.identity) as Transform;
 			BulletBehaviour bul = bullet.GetComponent<BulletBehaviour>();
 			bul.origin = transform;
