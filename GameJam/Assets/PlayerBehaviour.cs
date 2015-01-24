c428f607a351d1114f23363f55c93bc1a797ae13
RobertBroersma <Robert_broersma@hotmail.com>
2015-01-24 12:06:21 +0100

    New Scene and playerscript with movement and jamp

---
+++ GameJam/Assets/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float xmove = Input.GetAxis("Horizontal_P1");
+
+		rigidbody2D.AddForce(new Vector2(xmove * 10f, 0));
+	}
+
+	void OnCollisionStay2D(Collision2D col) {
+		if (col.gameObject.tag == "Ground"){
+			if (Input.GetButton("Jump_P1")) {
+				rigidbody2D.AddForce(new Vector2(0, 100f));
+			}
+		}
+	}
+}
