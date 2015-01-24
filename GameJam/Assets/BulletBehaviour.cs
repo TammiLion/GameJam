b56b9a0c421c333c79ca39ddc028fd4687b26548
RobertBroersma <Robert_broersma@hotmail.com>
2015-01-24 15:25:06 +0100

    Ranged, elements and weapons

---
+++ GameJam/Assets/BulletBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehaviour : MonoBehaviour {
+	private int dir = 1;
+	public float dmg = 10f;
+	public Transform origin;
+	public string Weapon = "Ranged";
+	public string Element;
+
+
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.AddForce(new Vector2(dir * 500f, 0f));
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
