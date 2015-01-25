e865e5dd07229ea3c96df638680b369668ade73c
MauritsRidder <maurits.ridder@hotmail.com>
2015-01-25 09:46:19 +0100

    Changes yesterday

---
+++ GameJam/Assets/HPBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HPBehaviour : MonoBehaviour {
+	private float maxHP = 100f;
+	private float maxW = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetHP (float HP) {
+		transform.localScale = new Vector3((HP / 100f) * maxW, 0.5f, 1f);
+	}
+}
