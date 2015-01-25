3c928c1503cf2d863f04bdf0c9d4d0dbdd025c72
MauritsRidder <maurits.ridder@hotmail.com>
2015-01-25 12:05:52 +0100

    Modern level finished
    Prehistory level also finished

---
+++ GameJam/Assets/GroupAnimationScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroupAnimationScript : MonoBehaviour {
+
+	bool left = true;
+	bool up = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 position = transform.position;
+		if (left) {
+			if (position.x < -16) {
+				left = false;
+			}
+			position.x-=(Time.deltaTime/2);
+		} else {
+			if (position.x > 16) {
+				left = true;
+			}
+			position.x+=(Time.deltaTime/2);
+		}
+
+		if (up) {
+			if (position.y < 5.3) {
+				up = false;
+			}
+			position.y -= (Time.deltaTime / 6);
+		} else {
+			if (position.y > 5.5) {
+				up = true;
+			}
+			position.y += (Time.deltaTime / 6);
+		}
+
+		transform.position = position;
+	}
+}
